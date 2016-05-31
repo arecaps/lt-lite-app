c23527bf99c80b871c13237f6e3a84ed332942d4
arecaps <arecaplan@gmail.com>
2016-05-31 22:43:06 +0000

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    //always require semicolons
+    "semi": [2, "always"],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import App from './App.vue';
+import Hello from './components/Hello.vue';
+
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+// We want to apply VueResource and VueRouter
+// to our Vue instance
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+
+const router = new VueRouter();
+
+// Pointing routes to the components they should use
+router.map({
+  '/hello': {
+    component: Hello,
+  },
+});
+
+// Any invalid route will redirect to home
+router.redirect({
+  '*': '/hello',
+});
+
+router.start(App, '#app');
+
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App },
+});
